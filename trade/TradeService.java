97f07177a694490880c0da825c7b04dc85e25076
Saahil-Shaikh <57488507+Saahil-Shaikh@users.noreply.github.com>
2022-06-10 19:32:38 +0530

    Add files via upload

---
+++ trade/TradeService.java
@@ -0,0 +1,39 @@
+package trade;
+
+//Sample Trade Ids:
+//London : 121456903
+//Newyork : 12232140
+//Tokyo : 12345678
+public class TradeService {
+
+	private RegionalDaoManager regionalDaoManager;
+	
+	public TradeService(RegionalDaoManager regionalDaoManager) {
+		this.regionalDaoManager = regionalDaoManager;
+	}
+	
+	public TradeService() {
+		
+	}
+	
+	public TradeDetails getTradeDetails(long tradeId) {
+		Region region = getTradeRegion(tradeId);
+		RegionalDao regionalDao = regionalDaoManager.getRegionalDao(region);
+		TradeDetails tradeDetails = regionalDao.getTradeDetails(tradeId);
+		tradeDetails.setRegion(region);
+		return tradeDetails;
+	}
+
+	public Region getTradeRegion(long tradeId) {
+		int regionCode = Integer.parseInt(String.valueOf(String.valueOf(tradeId).charAt(2)));
+		switch (regionCode) {
+		case 1:
+			return Region.LONDON;
+		case 2:
+			return Region.NEWYORK;
+		case 3:
+			return Region.TOKYO;
+		}
+		throw new IllegalArgumentException("Invalid TradeId");
+	}
+}
